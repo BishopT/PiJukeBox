2cfa2bcdb852914ba185372366878a9b2f988838
Thomas Rouillard <smothierry51420@gmail.com>
2020-03-26 00:45:29 +0100

    save current status on raspaudio branch

---
+++ pijukebox_output/.br-external.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file; DO NOT EDIT.
+#
+
+BR2_EXTERNAL ?= /home/thomas/Documents/projects/PiJukeBox
+BR2_EXTERNAL_NAMES = 
+BR2_EXTERNAL_DIRS = 
+BR2_EXTERNAL_MKS = 
+
+BR2_EXTERNAL_NAMES += PIJUKEBOX
+BR2_EXTERNAL_DIRS += /home/thomas/Documents/projects/PiJukeBox
+BR2_EXTERNAL_MKS += /home/thomas/Documents/projects/PiJukeBox/external.mk
+export BR2_EXTERNAL_PIJUKEBOX_PATH = /home/thomas/Documents/projects/PiJukeBox
+export BR2_EXTERNAL_PIJUKEBOX_DESC = PIJUKEBOX
